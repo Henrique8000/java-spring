4a05f4a3e920d8da29f05c09e8c80fddb65e7642
Henrique <henriqueflavioguimaraes@gmail.com>
2025-06-23 22:28:32 -0300

    feat: adicionando metodo POST (ainda sem nada funcional)

--- src/main/java/com/example/apitestetmeter/controller/ApiController.java
+++ src/main/java/com/example/apitestetmeter/controller/ApiController.java
@@ -2,6 +2,7 @@ package com.example.apitestetmeter.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,10 @@ public class ApiController {
 	@GetMapping(path = "/soma")
 	public ResponseEntity<Integer> exibeSoma() {
 		return ResponseEntity.ok(2 + 2);
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> coisaACoisa() {
+		return ResponseEntity.ok("coisando a coisa");
 	}
 }
